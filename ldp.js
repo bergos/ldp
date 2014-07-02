2feb41741bf97a93e043f58f975c9da395bd6c4c
bergi <bergi@axolotlfarm.org>
2014-07-02 18:26:07 +0200

    JSHint code cleanup

--- ldp.js
+++ ldp.js
@@ -1,162 +1,182 @@
-var Ldp = function(rdf, store, options) {
-	var self = this;
+var Ldp = function (rdf, store, options) {
+  var self = this;
 
-	if(options == null) options = {};
+  if (options == null) {
+    options = {};
+  }
 
-	self.error = {};
-	self.error.forbidden = function(req, res) { res.writeHead(403); res.end('<h1>Forbidden</h1>'); };
-	self.error.notFound = function(req, res) { res.writeHead(404); res.end('<h1>Not Found</h1>'); };
-	self.error.methodNotAllowed = function(req, res) { res.writeHead(405); res.end('<h1>Method Not Allowed</h1>'); };
-	self.error.notAcceptable = function(req, res) { res.writeHead(406); res.end('<h1>Not Acceptable</h1>'); };
+  self.error = {};
+  self.error.forbidden = function (req, res) { res.writeHead(403); res.end('<h1>Forbidden</h1>'); };
+  self.error.notFound = function (req, res) { res.writeHead(404); res.end('<h1>Not Found</h1>'); };
+  self.error.methodNotAllowed = function (req, res) { res.writeHead(405); res.end('<h1>Method Not Allowed</h1>'); };
+  self.error.notAcceptable = function (req, res) { res.writeHead(406); res.end('<h1>Not Acceptable</h1>'); };
 
-	self.log = 'log' in options ? options.log : function() {};
-	self.defaultAgent = 'defaultAgent' in options ? options.defaultAgent : null;
-	self.parsers = {};
-	self.parsers['application/ld+json'] = rdf.parseJsonLd;
-	self.parsers['text/turtle'] = rdf.parseTurtle;
+  self.log = 'log' in options ? options.log : function () {};
+  self.defaultAgent = 'defaultAgent' in options ? options.defaultAgent : null;
+  self.parsers = {};
+  self.parsers['application/ld+json'] = rdf.parseJsonLd;
+  self.parsers['text/turtle'] = rdf.parseTurtle;
 
-	self.serializers = {};
-	self.serializers['text/turtle'] = rdf.serializeNTriples;
-	self.serializers['application/ld+json'] = function(g,c){rdf.serializeJsonLd(g,function(j){c((JSON.stringify(j)));})};
+  self.serializers = {};
+  self.serializers['text/turtle'] = rdf.serializeNTriples;
+  self.serializers['application/ld+json'] = function (g, c) {
+    rdf.serializeJsonLd(g,
+      function (j) {
+        c((JSON.stringify(j)));
+      });
+  };
 
-	self.serializers.find = function(field) {
-		var keys = Object.keys(self.serializers);
+  self.serializers.find = function (field) {
+    var keys = Object.keys(self.serializers);
 
-		for(var i=0; i<keys.length; i++) {
-			if(field.indexOf(keys[i]) >= 0)
-				return keys[i];
-		};
+    for (var i = 0; i < keys.length; i++) {
+      if (field.indexOf(keys[i]) >= 0) {
+        return keys[i];
+      }
+    }
 
-		return null;
-	};
+    return null;
+  };
 
-	self.parsers.find = function(field) {
-		var keys = Object.keys(self.parsers);
+  self.parsers.find = function (field) {
+    var keys = Object.keys(self.parsers);
 
-		for(var i=0; i<keys.length; i++) {
-			if(field.indexOf(keys[i]) >= 0)
-				return keys[i];
-		};
+    for (var i = 0; i < keys.length; i++) {
+      if (field.indexOf(keys[i]) >= 0) {
+        return keys[i];
+      }
+    }
 
-		return null;
-	};
+    return null;
+  };
 
-	self.requestIri = function(req) {
-		return 'http://localhost' + req.url; //TODO
-	};
+  self.requestIri = function (req) {
+    return 'http://localhost' + req.url; //TODO
+  };
 
-	self.middleware = function(req, res, next) {
-		var iri = self.requestIri(req);
-		var agent = self.defaultAgent;
-		var application = null;
+  self.middleware = function (req, res, next) {
+    var iri = self.requestIri(req);
+    var agent = self.defaultAgent;
+    var application = null;
 
-		if(('session' in req) &&  ('agent' in req.session) && (req.session.agent != null))
-			agent = req.session.agent;
+    if (('session' in req) &&  ('agent' in req.session) && (req.session.agent != null)) {
+      agent = req.session.agent;
+    }
 
-		if(typeof req.headers.origin !== 'undefined')
-			application = req.headers.origin;
+    if (typeof req.headers.origin !== 'undefined') {
+      application = req.headers.origin;
+    }
 
-		self.log((new Date()).toISOString() + ' ' + req.method + ': ' + iri + ' ' + agent + ' ' + application);
+    self.log((new Date()).toISOString() + ' ' + req.method + ': ' + iri + ' ' + agent + ' ' + application);
 
-		if(req.method == 'GET') {
-			self.get(req, res, next, iri, agent, application);
-		} else if(req.method == 'HEAD') {
-			self.get(req, res, next, iri, agent, application, true);
-		} else if(req.method == 'PATCH') {
-			self.patch(req, res, next, iri, agent, application);
-		} else if(req.method == 'PUT') {
-			self.put(req, res, next, iri, agent, application);
-		} else if(req.method == 'DELETE') {
-			self.del(req, res, next, iri, agent, application);
-		} else {
-			self.error.methodNotAllowed(req, res, next);
-		}
-	};
+    if (req.method == 'GET') {
+      self.get(req, res, next, iri, agent, application);
+    } else if (req.method == 'HEAD') {
+      self.get(req, res, next, iri, agent, application, true);
+    } else if (req.method == 'PATCH') {
+      self.patch(req, res, next, iri, agent, application);
+    } else if (req.method == 'PUT') {
+      self.put(req, res, next, iri, agent, application);
+    } else if (req.method == 'DELETE') {
+      self.del(req, res, next, iri, agent, application);
+    } else {
+      self.error.methodNotAllowed(req, res, next);
+    }
+  };
 
-	self.get = function(req, res, next, iri, agent, application, skipBody) {
-		var mimeType = self.serializers.find(req.headers.accept);
+  self.get = function (req, res, next, iri, agent, application, skipBody) {
+    var mimeType = self.serializers.find(req.headers.accept);
 
-		if(mimeType == null)
-			return self.error.notAcceptable(req, res, next);
+    if (mimeType == null) {
+      return self.error.notAcceptable(req, res, next);
+    }
 
-		store.graph(iri, function(graph) {
-			if(graph == null)
-				return self.error.notFound(req, res, next);
+    store.graph(iri, function (graph) {
+      if (graph == null) {
+        return self.error.notFound(req, res, next);
+      }
 
-			self.serializers[mimeType](graph, function(data) {
-				res.statusCode = 200; // OK
-				res.setHeader('Content-Type', mimeType);
+      self.serializers[mimeType](graph, function (data) {
+        res.statusCode = 200; // OK
+        res.setHeader('Content-Type', mimeType);
 				
-				if (!skipBody) {
-					res.write(data);
-				}
+        if (!skipBody) {
+          res.write(data);
+        }
 
-				res.end();
-			}, iri);
-		}, agent, application);
-	};
+        res.end();
+      }, iri);
+    }, agent, application);
+  };
 
-	self.patch = function(req, res, next, iri, agent, application) {
-		var mimeType = self.parsers.find(req.headers['content-type']);
+  self.patch = function (req, res, next, iri, agent, application) {
+    var mimeType = self.parsers.find(req.headers['content-type']);
 
-		if(mimeType == null)
-			return self.error.notAcceptable(req, res, next);
+    if (mimeType == null) {
+      return self.error.notAcceptable(req, res, next);
+    }
 
-		var content = '';
+    var content = '';
 
-		req.on('data', function(data) {
-			content += data.toString();
-		}).on('end', function() {
-			self.parsers[mimeType](content, function(graph) {
-				if(graph == null)
-					return self.error.notAcceptable(req, res, next);
+    req.on('data', function (data) {
+      content += data.toString();
+    }).on('end', function () {
+      self.parsers[mimeType](content, function (graph) {
+        if (graph == null) {
+          return self.error.notAcceptable(req, res, next);
+        }
 
-				store.merge(iri, graph, function(merged) {
-					if(merged == null)
-						return self.error.forbidden(req, res, next);
+        store.merge(iri, graph, function (merged) {
+          if (merged == null) {
+            return self.error.forbidden(req, res, next);
+          }
 
-					res.statusCode = 204; // No Content
-					res.end();
-				}, agent, application);
-			}, iri);
-		});
-	};
+          res.statusCode = 204; // No Content
+          res.end();
+        }, agent, application);
+      }, iri);
+    });
+  };
 
-	self.put = function(req, res, next, iri, agent, application) {
-		var mimeType = self.parsers.find(req.headers['content-type']);
+  self.put = function (req, res, next, iri, agent, application) {
+    var mimeType = self.parsers.find(req.headers['content-type']);
 
-		if(mimeType == null)
-			return next();
+    if (mimeType == null) {
+      return next();
+    }
 
-		var content = '';
+    var content = '';
 
-		req.on('data', function(data) {
-			content += data.toString();
-		}).on('end', function() {
-			self.parsers[mimeType](content, function(graph) {
-				if(graph == null)
-					return self.error.notAcceptable(req, res);
+    req.on('data', function (data) {
+      content += data.toString();
+    }).on('end', function () {
+      self.parsers[mimeType](content, function (graph) {
+        if (graph == null) {
+          return self.error.notAcceptable(req, res);
+        }
 
-				store.add(iri, graph, function(added) {
-					if(added == null)
-						return self.error.forbidden(req, res, next);
+        store.add(iri, graph, function (added) {
+          if (added == null) {
+            return self.error.forbidden(req, res, next);
+          }
 
-					res.statusCode = 204; // No Content
-					res.end();
-				}, agent, application);
-			}, iri);
-		});
-	};
+          res.statusCode = 204; // No Content
+          res.end();
+        }, agent, application);
+      }, iri);
+    });
+  };
 
-	self.del = function(req, res, next, iri, agent, application) {
-		store.delete(iri, function(success) {
-			if(!success)
-				return self.error.forbidden(req, res, next);
+  self.del = function (req, res, next, iri, agent, application) {
+    store.delete(iri, function (success) {
+      if (!success) {
+        return self.error.forbidden(req, res, next);
+      }
 
-			res.statusCode = 204; // No Content
-			res.end();
-		}, agent, application);
-	};
+      res.statusCode = 204; // No Content
+      res.end();
+    }, agent, application);
+  };
 };
 
 
